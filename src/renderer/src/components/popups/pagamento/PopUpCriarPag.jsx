97304f641de9e8803d409eb1270352a0cbce5965
henrickviana <henrick.viana16@outlook.com>
2025-10-23 08:59:26 -0300

    popupcriarpag

---
+++ src/renderer/src/components/popups/pagamento/PopUpCriarPag.jsx
@@ -0,0 +1,95 @@
+import { IoMdClose } from 'react-icons/io'
+import { useEffect, useState, useRef } from 'react'
+import Button from '../../botoes/DesignBotao'
+import { IoMdArrowRoundBack, IoMdArrowRoundForward, IoIosArrowDown } from 'react-icons/io'
+
+export default function PopupCriarPag({ showModal, onClose, insertTable }) {
+  if (!showModal) return null
+
+  const PAGE_SIZE = 5
+  const [columns, setColumns] = useState([])
+  const [selectOptions, setSelectOptions] = useState([])
+  const [page, setPage] = useState(1)
+  const [userDropdownOpen, setUserDropdownOpen] = useState(false)
+  const [pagamentosBase, setProdutosBase] = useState([])
+  const [baseValue, setBaseValue] = useState('')
+  const userDropdownRef = useRef(null)
+
+  {/* 
+  useEffect(() => {
+    setPage(1)
+    window.api
+      .getProdutosNomes()
+      .then((result) => {
+        setProdutosBase(result)
+      })
+      .catch((error) => {
+        console.error('Error fetching product names:', error)
+      })
+    window.api
+      .getPedidoProdutosByStatus('Em analise')
+      .then((cols) => {
+        setColumns(Object.keys(cols[0]).filter((col) => col !== 'Id'))
+      })
+      .catch((error) => {
+        console.error('Error fetching table columns:', error)
+      })
+  }, [insertTable])
+*/}
+  const totalPages = Math.ceil(columns.length / PAGE_SIZE)
+  const startIdx = (page - 1) * PAGE_SIZE
+  const endIdx = startIdx + PAGE_SIZE
+  const inputs = columns.slice(startIdx, endIdx)
+  const inputNumbers = ['ML', 'Preço', 'Peso', 'Quantidade']
+
+  return (
+    <>
+      <div className="fixed inset-0 bg-black/40 z-10"></div>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-[#1A6D12] w-200 z-20 h-180 p-4 bg-[#fffcff] rounded-xl shadow-2xl">
+        <div className="flex justify-end">
+          <button className="cursor-pointer" onClick={onClose}>
+            <IoMdClose />
+          </button>
+        </div>
+
+        <h1 className="mt-4 text-4xl text-[#1A6D12] font-black py-4 text-center">Novo Pagamento</h1>
+
+        <div className="flex flex-col justify-between h-140">
+          <div>
+            <div className="mt-7 flex flex-col px-30 h-90 w-full">
+
+              {/* Aqui no futuro entram os inputs puxados do banco */}
+              {inputs.map((coluna, index) => (
+                <input
+                  key={index}
+                  type={inputNumbers.includes(coluna) ? 'number' : 'text'}
+                  placeholder={coluna}
+                  className="border border-[#1A6D12] px-4 py-2 rounded-xl mt-3 focus:outline-none focus:ring-2 focus:ring-[#1A6D12] text-gray-800 placeholder-gray-500"
+                />
+              ))}
+            </div>
+
+            {totalPages > 1 && (
+              <div className="flex justify-end w-full px-30 mt-2">
+                <Button
+                  onClick={() => setPage(page - 1)}
+                  className={`${page === 1 ? 'hidden' : 'text-[#1A6D12] border-solid border border-[#1A6D12] hover:bg-[#ececec]'}`}
+                  text={<IoMdArrowRoundBack />}
+                />
+                <Button
+                  onClick={() => setPage(page + 1)}
+                  className={`${page === totalPages ? 'hidden' : 'text-[#1A6D12] border-solid border border-[#1A6D12] hover:bg-[#ececec]'}`}
+                  text={<IoMdArrowRoundForward />}
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="flex justify-center">
+            <Button className="text-white bg-[#1A6D12] hover:bg-[#145A0C] w-60" text="Salvar" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/renderer/src/pages/Pagamento.jsx
+++ src/renderer/src/pages/Pagamento.jsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react'
 import Tabela from '../components/tabelas/Tabela'
 import Input from '../components/inputs/Input'
 import Button from '../components/botoes/DesignBotao'
+import Popup from '../components/popups/pagamento/PopUpCriarPag'
 
 export default function Pagamento() {
   const [data, setData] = useState([]) // dados do banco
   const [filteredData, setFilteredData] = useState([]) // dados filtrados
   const [term, setTerm] = useState('') // termo de pesquisa
   const [tipo, setTipo] = useState('Registrado') // tipo selecionado
+  const [showModal, setShowModal] = useState(false)
+  
+  const insertTable = 'pagamento'
 
   // Busca os dados do banco sempre que o tipo mudar
   useEffect(() => {
@@ -25,6 +29,9 @@ export default function Pagamento() {
       data.filter(f => f.Nome.toLowerCase().includes(term.toLowerCase()))
     )
   }, [data, term])
+
+  const openModal = () => setShowModal(true)
+  const closeModal = () => setShowModal(false)
 
   const changeData = (novoTipo) => setTipo(novoTipo)
 
@@ -46,61 +53,57 @@ export default function Pagamento() {
   }))
 
   return (
-    <div className="pt-10 flex flex-col items-center">
+    <div className="pt-10">
       <h1 className="text-4xl text-[#1A6D12] font-black py-4 text-center">
         Pagar Funcionários
       </h1>
-
-      <div className="w-full max-w-5xl mt-10">
-        <p className="text-black mb-2 text-lg font-medium">Funcionários:</p>
-
-        {/* Barra de pesquisa e botão */}
-        <div className="w-full flex items-center justify-between mb-4">
-          <div className="flex w-1/3">
+      {/* texto do input da tabela */}
+      <p className="text-black ps-30 mt-10 mb-3">Nome do Funcionário:</p>
+      {/* container da tabela */}
+      <div className=" w-full px-30">
+        {/* table options */}
+        <div className="w-full flex justify-between">
+          {/* Name */}
+          <div className="flex flex-col w-1/3">
             <Input
               inputType="text"
-              placeholder="Buscar..."
+              placeholder="Nome"
               inputName="nome"
               onChange={(e) => setTerm(e.target.value)}
             />
           </div>
+          {/* options */}
           <div className="flex gap-3">
             <Button
-              className="text-white bg-[#1A6D12] hover:bg-[#145A0C] w-40 py-2"
+              className="text-white bg-[#1A6D12] hover:bg-[#145A0C] w-40 h-full py-2"
               text="Adicionar"
-              onClick={() => console.log('Abrir formulário para registrar dias')}
+              onClick={openModal}
             />
           </div>
         </div>
-
-        {/* Tabela */}
-        <div className="border border-[#1A6D12] h-120 overflow-auto w-full rounded-lg mx-auto">
+        {/* table */}
+        <div className="border border-[#1A6D12] h-120 overflow-auto w-full mt-3">
           <Tabela
             data={tabelaFormatada}
-            columns={['Nome', 'Cargo', 'Calendário', 'Valor a Pagar', 'Observações', 'Data do Pagamento']}
+            columns={["Nome", "Cargo", "Calendário", "Valor a Pagar", "Observações", "Data do Pagamento"]}
+            insertTable={insertTable}
           />
         </div>
-
-        {/* Escolher entre tabelas */}
+        {/* escolher entre tabelas */}
         <div className="mt-4 mb-4 flex justify-around">
           <Button
             onClick={() => changeData('Registrado')}
-            className={`${tipo === 'Registrado'
-              ? 'text-white bg-[#1A6D12] hover:bg-[#145A0C]'
-              : 'text-[#1A6D12] border border-[#1A6D12] hover:bg-[#ececec]'
-            } w-60`}
+            className={`${tipo === 'Registrado' ? 'text-white bg-[#1A6D12] hover:bg-[#145A0C]' : 'text-[#1A6D12] border-solid border border-[#1A6D12] hover:bg-[#ececec]'} w-60`}
             text="Registrado"
           />
           <Button
             onClick={() => changeData('Não Registrado')}
-            className={`${tipo === 'Não Registrado'
-              ? 'text-white bg-[#1A6D12] hover:bg-[#145A0C]'
-              : 'text-[#1A6D12] border border-[#1A6D12] hover:bg-[#ececec]'
-            } w-60`}
+            className={`${tipo === 'Não Registrado' ? 'text-white bg-[#1A6D12] hover:bg-[#145A0C]' : 'text-[#1A6D12] border-solid border border-[#1A6D12] hover:bg-[#ececec]'} w-60`}
             text="Não Registrado"
           />
         </div>
       </div>
+      <Popup showModal={showModal} onClose={closeModal} table={data} insertTable={insertTable} />
     </div>
   )
 }
